8936a6fa2d3a3218e66c8a2670538e46a40c9b96
yhnujk <145305956+yhnujk@users.noreply.github.com>
2023-11-27 09:57:28 +0900

    Add files via upload

---
+++ Shopmall/src/com/market/cart/CartInterface.java
@@ -0,0 +1,13 @@
+package com.market.cart;
+
+import java.util.ArrayList;
+import com.market.bookitem.Book;
+
+public interface CartInterface {
+
+	void printBookList(ArrayList<Book> p);
+	boolean isCartInBook(String id);
+	void insertBook(Book p);
+	void removeCart(int numId);
+	void deleteBook();
+}
